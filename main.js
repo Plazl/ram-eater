42f6d95ef25e561c587193af214b649c133311c8
jhnbg rth <I1234itsplaz@outlook.com>
2022-04-28 12:41:55 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+var var1 = 1;
+var var2 = 1;
+var var3 = 1;
+var var4 = 1;
+function start() {
+ while (true) {
+  while (true) {
+   while (true) {
+    while (true) {
+     while (true) {
+      while (true) {
+       while (true) {
+        while (true) {
+         while (true) {
+          while (true) {
+           while (true) {
+            while (true) {
+              var1 += var1 + var2 * var3 / var4
+              var2 += var2 + var3 * var4 / var1
+              var3 += var3 + var4 * var1 / var2
+              var4 += var4 + var1 * var2 / var3
+              document.getElementById('var1').innerHTML = var1
+              document.getElementById('var2').innerHTML = var2
+              document.getElementById('var3').innerHTML = var3
+              document.getElementById('var4').innerHTML = var4
+            }
+           }
+          }
+         }
+        }
+       }
+      }
+     }
+    }
+   } 
+  }
+ }
+}
